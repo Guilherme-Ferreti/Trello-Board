074959dd34ce50b87cd4297440d2f5c965108757
Guilherme Ferreti <guiferretis@gmail.com>
2025-11-03 16:55:09 -0300

    feat: add deploy command

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false,
+  app: {
+    baseURL: '/Trello-Board/'
+  },
   modules: ['@vueuse/nuxt'],
   css: ['./app/assets/css/main.css'],
   vite: {
