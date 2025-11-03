811352ee59fd0caed29960e53e89358ba149122b
Guilherme Ferreti <guiferretis@gmail.com>
2025-11-03 16:23:09 -0300

    feat: add app logo

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="p-2.5 h-screen bg-teal-600 overflow-auto">
-    <h1 class="text-4xl text-white mb-2.5">Trello Board</h1>
-    <TrelloBoard />
+    <header class="flex gap-1 text-white items-center mb-2.5">
+      <IconBrandNuxt class="size-3" />
+      <h1 class="text-4xl">Trello Board</h1>
+    </header>
+    <main>
+      <TrelloBoard />
+    </main>
   </div>
 </template>
+
+<script setup lang="ts">
+import { IconBrandNuxt } from '@tabler/icons-vue';
+
+useSeoMeta({
+  title: 'Trello Board'
+})
+</script>
